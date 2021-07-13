4806a3f1a44ff4d2e344a6a94de6f7f1844935e3
Rahul Patil <rahulpatil.m30@gmail.com>
2021-07-13 15:35:04 +0530

    initial referral component with tests

--- src/components/referral/Referral.js
+++ src/components/referral/Referral.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Referral.css';
 
 const Referral = () => {
+
+    const [ referral, setReferral ] = useState('');
+    const [ isChecked, setIsChecked ] = useState(false);
+
+    const handleInputChange = (e) => {
+        setIsChecked(e.target.checked);
+    }
+
     return (
-        <div>
-            
-        </div>
+        <form
+            className="signup-form" 
+        >
+            <h3>
+                Enter ~ Referral Code   
+            </h3>
+            <input 
+                id="referral" 
+                data-testid="referral"
+                type='text' 
+                value={referral}
+                onChange={e => setReferral(e.target.value)}
+            />
+            <div>
+                <input 
+                    name="referral-check"
+                    id="referral-check"
+                    type="checkbox"
+                    data-testid="referral-check"
+                    checked={isChecked}
+                    onChange={handleInputChange}
+                />
+                <label 
+                    htmlFor="referral-check"
+                >
+                    I don't have a referral code
+                </label>
+            </div>
+            <button
+                className="btn"
+                id="btn"
+                data-testid="btn"
+                disabled={!isChecked && !referral}
+            >
+                Submit
+            </button>
+        </form>
     )
 }
 
